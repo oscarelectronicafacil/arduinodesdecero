50d79cd060937c527b22c3151ed714e97a3b2a35
oscarelectronicafacil <oscarelectronicafacil@gmail.com>
2025-12-11 13:16:12 +0000

    Add files via upload

---
+++ ldc-display/lcd-alarma/lcd-alarma.ino
@@ -0,0 +1,22 @@
+//#include <Adafruit_LiquidCrystal.h>
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7,6,5,4,3,2);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  lcd.setCursor(0,0);
+  lcd.print("Ingrese clave:");
+  lcd.setCursor(0,1);
+  lcd.cursor();
+  delay(500);
+  lcd.noCursor();
+  delay(500);  
+}
